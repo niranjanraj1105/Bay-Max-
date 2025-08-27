ff0b5a65531e5820cad643831eee5b019345f757
niranjanraj1105 <niranjanraj1105@gmail.com>
2025-08-27 21:16:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,5 @@ def chat():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
